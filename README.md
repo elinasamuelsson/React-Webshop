f29fde60451faa1dc2987bd70f5d68aaf8475a7b
elinasamuelsson <elina.samuelsson@live.com>
2026-08-27 08:28:27 +0200

    initierar projektet
    Initierar ett React-projekt att bygga vidare på genom Vite.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # React-Webshop
+
+Grupp 4s webshop i React.
+
+Gruppmedlemmar: Elina, Alex, Hussein, Admir
---
+++ react-webshop/src/App.jsx
@@ -0,0 +1,14 @@
+import {useState} from "react";
+import "./App.css";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<h3>Hello World!</h3>
+		</>
+	);
+}
+
+export default App;
