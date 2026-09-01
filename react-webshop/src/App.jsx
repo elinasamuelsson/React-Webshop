042b1c611049df16585e8376a7400654cd9cf130
Hussein <hossein.abrahim2217@gmail.com>
2026-09-01 17:31:38 +0200

    Changed branch name

--- react-webshop/src/App.jsx
+++ react-webshop/src/App.jsx
@@ -1,12 +1,14 @@
 import {useState} from "react";
 import "./App.css";
+import Footer from "./components/Footer.jsx";
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
-			<h3>Hello World!</h3>
+			{/* <h3>Hello World!</h3> */}
+			<Footer />
 		</>
 	);
 }
