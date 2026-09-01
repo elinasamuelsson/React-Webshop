9d48d8a2c9f4929bce035adfcbdda83882923730
Alex Reichstein <alex.reichstein1992@gmail.com>
2026-09-01 21:47:59 +0200

    Header-komponent med navigering admin, home, products

--- react-webshop/src/App.jsx
+++ react-webshop/src/App.jsx
@@ -1,4 +1,6 @@
 import {useState} from "react";
+import Header from "./components/Header";
+
 import "./App.css";
 
 function App() {
@@ -6,7 +8,7 @@ function App() {
 
 	return (
 		<>
-			<h3>Hello World!</h3>
+			<Header />
 		</>
 	);
 }
