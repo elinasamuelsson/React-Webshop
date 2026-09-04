383892c5ac4317c6829a71f775a5c1c0d18411e2
elinasamuelsson <elina.samuelsson@live.com>
2026-09-04 07:29:54 +0200

    Fixar typo där jag kallat på BasketContext istället för BasketProvider för att initiera kontexten för varukorgen

--- react-webshop/src/App.jsx
+++ react-webshop/src/App.jsx
@@ -1,16 +1,16 @@
 import {Outlet} from "react-router";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
-import {BasketContext} from "./context/basketContext.jsx";
+import {BasketProvider} from "./context/basketContext.jsx";
 
 function App() {
 	return (
 		<>
-			<BasketContext>
+			<BasketProvider>
 				<Header />
 				<Outlet />
 				<Footer />
-			</BasketContext>
+			</BasketProvider>
 		</>
 	);
 }
