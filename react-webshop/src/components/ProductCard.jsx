b281ef737e6d9a79506a1df1fac4a8d7471ad0ab
elinasamuelsson <elina.samuelsson@live.com>
2026-09-17 13:18:06 +0200

    Fixar problem där tidigare push la rader på fel ställe
    Produktkorten har nu Länkar till Productpage.jsx, och implementerar Toast-popups

--- react-webshop/src/components/ProductCard.jsx
+++ react-webshop/src/components/ProductCard.jsx
@@ -1,90 +1,103 @@
-import { useEffect, useState, useContext } from "react";
+import {useEffect, useState, useContext} from "react";
+import {Link} from "react-router";
 import Product from "../api/Products.js";
 import "./ProductCard.css";
 import DiscountIcon from "./DiscountIcon.jsx";
-import { BasketContext } from "../context/BasketContext.jsx";
+import {BasketContext} from "../context/BasketContext.jsx";
+import {ToastContext} from "../context/ToastContext.jsx";
 
-export default function Products({ filter }) {
+export default function Products({filter}) {
+	const productsAPI = new Product();
+	const [products, setProducts] = useState([]);
+	const [page, setPage] = useState(1);
+	const [totalPages, setTotalPages] = useState(1);
+	const {dispatch: basketDispatch} = useContext(BasketContext);
+	const {dispatch: toastDispatch} = useContext(ToastContext);
 
-    const productsAPI = new Product();
-    const [products, setProducts] = useState([]);
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
-    const { dispatch } = useContext(BasketContext);
+	useEffect(() => {
+		setPage(1);
+	}, [filter]);
 
-    useEffect(() => {
-        setPage(1);
-    }, [filter]);
+	async function getProducts() {
+		const {response, result} = await productsAPI.getAllProducts(page, filter);
 
-    async function getProducts() {
-        const { response, result } = await productsAPI.getAllProducts(page, filter);
+		if (response.ok) {
+			setProducts(result.data);
+			setTotalPages(result.pages);
+		} else {
+			console.log("Something went wrong while fetching products!");
+		}
+	}
 
-        if (response.ok) {
-            setProducts(result.data);
-            setTotalPages(result.pages);
-        } else {
-            console.log("Something went wrong while fetching products!");
-        }
-    }
+	useEffect(() => {
+		getProducts(page);
+	}, [page, filter]);
 
-    useEffect(() => {
-        getProducts(page);
-    }, [page, filter]);
+	// Här filtreras filmerna beroende på filtret
+	const displayedProducts = filter ? products.filter((p) => p.genre === filter) : products;
 
-    // Här filtreras filmerna beroende på filtret
-    const displayedProducts = filter ? products.filter(p => p.genre === filter) : products;
+	// Lägger till en produkt i varukorgen, alltid med antal 1 från produktgriden
+	function addToCart(product) {
+		basketDispatch({type: "ADD", payload: {product, productQuantity: 1}});
+		toastDispatch({type: "SHOW", payload: "Item added to cart!"});
+	}
 
-    // Lägger till en produkt i varukorgen, alltid med antal 1 från produktgriden
-    function addToCart(product) {
-        dispatch({ type: "ADD", payload: { product, productQuantity: 1 } });
-    }
+	return (
+		<div class="card-container">
+			{displayedProducts.map((p) => {
+				return (
+					<div class="card" key={p.id} style={{display: "flex", flexDirection: "column"}}>
+						{p.isDiscount ? <DiscountIcon /> : null}
 
-    return (
-        <div class="card-container">
-            {
-                displayedProducts.map(p => {
-                    return (
-                        <div class="card" key={p.id} style={{display: "flex", flexDirection: "column"}}>
+						<img class="card-img" alt={`${p.title}`} src={`./public/productimages/${p.imgLink}`} />
+						<p class="card-genre">{p.genre.toUpperCase()}</p>
+						<Link to={`/products/${p.id}`}>
+							<h3 class="card-title">{p.title}</h3>
+						</Link>
+						<p class="card-release">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="16"
+								height="16"
+								fill="currentColor"
+								class="bi bi-calendar4-event"
+								viewBox="0 0 16 16"
+							>
+								<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z" />
+								<path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z" />
+							</svg>
+							{p.release}
+						</p>
+						<p class="card-description">{p.description}</p>
+						<p class="card-stock">{p.stock}</p>
+						<div>
+							<p class="card-price">${p.price}</p>
+							<button onClick={() => addToCart(p)}>ADD TO CART</button>
+						</div>
+					</div>
+				);
+			})}
 
-                            { p.isDiscount ? <DiscountIcon /> : null}
-
-                            <img class="card-img" alt={`${p.title}`} src={`./public/productimages/${p.imgLink}`} />
-                            <p class="card-genre">{p.genre.toUpperCase()}</p>
-                            <h3 class="card-title">{p.title}</h3>
-                            <p class="card-release">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar4-event" viewBox="0 0 16 16">
-                                    <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z"/>
-                                    <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
-                                </svg>
-                                {p.release}
-                            </p>
-                            <p class="card-description">{p.description}</p>
-                            <p class="card-stock">{p.stock}</p>
-                            <div>
-                                <p class="card-price">${p.price}</p>
-                                <button onClick={() => addToCart(p)}>ADD TO CART</button>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-
-            {/* Knappar för att bläddra mellan sidorna */}
-            <div style={{gridColumn: "span 3", display: "flex", alignItems: "center", justifyContent: "center"}}>
-                <button class="previous"
-                    disabled={page <= 1}
-                    onClick={() => setPage(prev => prev - 1)} // Functional state value
-                >
-                    Previous
-                </button>
-                <span class="page">Page {page} of {totalPages}</span>
-                <button class="next"
-                    disabled={page >= totalPages || displayedProducts.length < 9} 
-                    onClick={() => setPage(prev => prev + 1)}
-                >
-                    Next
-                </button>
-            </div>
-        </div>
-    )
+			{/* Knappar för att bläddra mellan sidorna */}
+			<div style={{gridColumn: "span 3", display: "flex", alignItems: "center", justifyContent: "center"}}>
+				<button
+					class="previous"
+					disabled={page <= 1}
+					onClick={() => setPage((prev) => prev - 1)} // Functional state value
+				>
+					Previous
+				</button>
+				<span class="page">
+					Page {page} of {totalPages}
+				</span>
+				<button
+					class="next"
+					disabled={page >= totalPages || displayedProducts.length < 9}
+					onClick={() => setPage((prev) => prev + 1)}
+				>
+					Next
+				</button>
+			</div>
+		</div>
+	);
 }
